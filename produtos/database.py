5f958bee86f416c5b47aad436a3eabea726da58a
Ettore Mitsueda <ettore.mitsueda@luizalabs.com>
2022-12-14 16:35:08 -0300

    Refatorar código das rotas

--- produtos/database.py
+++ produtos/database.py
@@ -1,22 +1,21 @@
+from bson import ObjectId as OId
 from dotenv import dotenv_values
 from fastapi import APIRouter
 from pymongo import MongoClient
 
 from type_codecs import codec_options
 
-
 config = dotenv_values("produtos/.env")
 
+
 class DatabaseConnection:
-    def __init__(self,
-                 connection_string: str = config["CONNECTION_STRING"],
-                 database_name: str = config["DATABASE_NAME"],
-                 collection_name: str = config["COLLECTION_NAME"]):
-        self.client = MongoClient(connection_string, serverSelectionTimeoutMS=5000)
-        self.db = self.client.get_database(database_name)
-        self.coll = self.db.get_collection(collection_name, codec_options=codec_options)
+    def __init__(self):
+        self.client = MongoClient(config["CONNECTION_STRING"], serverSelectionTimeoutMS=5000)
+        self.db = self.client.get_database(config["DATABASE_NAME"])
+        self.coll = self.db.get_collection(config["CONNECTION_STRING"], codec_options=codec_options)
 
 mongo = DatabaseConnection()
+
 
 db_router = APIRouter()
 
--- produtos/routes.py
+++ produtos/routes.py
@@ -10,64 +10,61 @@ router = APIRouter(prefix="/produtos", tags=["Produtos"])
 
 @router.get("/", status_code=200, response_model=list[Produto])
 def listar_produtos():
-    return list(mongo.coll.find(projection={"_id": False}))
+    find_result = mongo.coll.find(projection={"_id": False})
+
+    return list(find_result)
 
 
 @router.get("/{doc_id}", status_code=200, response_model=Produto)
 def listar_um_produto(doc_id: int):
-    if doc := mongo.coll.find_one(
-            filter={"doc_id": doc_id},
-            projection={"_id": False}):
-        return doc
+    args = {
+        "filter": {"doc_id": doc_id},
+        "projection": {"_id": False}}
+
+    if find_result := mongo.coll.find_one(**args):
+        return find_result
 
     raise HTTPException(404, f"Produto com o ID {doc_id} não existe")
 
 
 @router.post("/", status_code=201, response_model=Produto)
 def cadastrar_produto(produto: Produto = Body(examples=schema.post)):
-    produto.doc_id = mongo.coll.count_documents({}) + 1
+    produto.doc_id += mongo.coll.count_documents({})
     new_doc = produto.dict(exclude={"_id": True})
 
-    if mongo.coll.find_one({k:v for k,v in new_doc.items() if k != "doc_id"}):
-        raise HTTPException(400, "Este produto já está cadastrado")
+    insert_result = mongo.coll.insert_one(new_doc)
+    args = {
+        "filter": {"_id": insert_result.inserted_id},
+        "projection": {"_id": False}}
 
-    result = mongo.coll.insert_one(new_doc)
-    inserted_doc = mongo.coll.find_one(
-        filter={"_id": result.inserted_id},
-        projection={"_id": False})
-
+    inserted_doc = mongo.coll.find_one(**args)
     return inserted_doc
 
 
-@router.patch(
-    "/{doc_id}",
-    status_code=200,
-    response_model=ProdutoResponse,
-    response_model_exclude_unset=True)
+@router.patch("/{doc_id}", status_code=200,
+              response_model=ProdutoResponse, response_model_exclude_unset=True)
 def atualizar_produto(doc_id: int, produto: ProdutoPatchReq = Body(examples=schema.patch)):
     new_values = produto.dict(exclude={"_id": True, "doc_id": True}, exclude_unset=True)
 
-    if doc_before := mongo.coll.find_one_and_update(
-            filter={"doc_id": doc_id},
-            update={"$set": new_values},
-            projection={"_id": False}):
-        doc_after = mongo.coll.find_one(
-            filter={"doc_id": doc_id},
-            projection={"_id": False})
+    args = {
+        "filter": {"doc_id": doc_id},
+        "projection": {"_id": False}}
+
+    if doc_before := mongo.coll.find_one_and_update(**args, update={"$set": new_values}):
+        doc_after = mongo.coll.find_one(**args)
         return ProdutoResponse(antes=doc_before, depois=doc_after)
     
     raise HTTPException(404, f"Produto com o ID {doc_id} não existe")
 
 
-@router.delete(
-    "/{doc_id}",
-    status_code=200,
-    response_model=ProdutoResponse,
-    response_model_exclude_unset=True)
+@router.delete("/{doc_id}", status_code=200,
+               response_model=ProdutoResponse, response_model_exclude_unset=True)
 def deletar_produto(doc_id: int):
-    if deleted_doc := mongo.coll.find_one_and_delete(
-            filter={"doc_id": doc_id},
-            projection={"_id": False}):
+    args = {
+        "filter": {"doc_id": doc_id},
+        "projection": {"_id": False}}
+
+    if deleted_doc := mongo.coll.find_one_and_delete(**args):
         return ProdutoResponse(deletado=deleted_doc)
     
     raise HTTPException(404, f"Produto com o ID {doc_id} não foi deletado, pois não existe")
