7f9e20dbc9fc7d81b98bd81ff8a8387fb7a1d270
Ettore Mitsueda <ettore.mitsueda@luizalabs.com>
2022-12-14 16:39:58 -0300

    bugfix: usar nome correto da collection

--- produtos/database.py
+++ produtos/database.py
@@ -1,4 +1,4 @@
-from bson import ObjectId as OId
+from bson import ObjectId
 from dotenv import dotenv_values
 from fastapi import APIRouter
 from pymongo import MongoClient
@@ -12,7 +12,7 @@ class DatabaseConnection:
     def __init__(self):
         self.client = MongoClient(config["CONNECTION_STRING"], serverSelectionTimeoutMS=5000)
         self.db = self.client.get_database(config["DATABASE_NAME"])
-        self.coll = self.db.get_collection(config["CONNECTION_STRING"], codec_options=codec_options)
+        self.coll = self.db.get_collection(config["COLLECTION_NAME"], codec_options=codec_options)
 
 mongo = DatabaseConnection()
 
